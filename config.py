11e97b8192899fe98def191c7617c20d710f0ae2
0xHambaAllah <118465539+0xHambaAllah@users.noreply.github.com>
2025-10-24 13:01:32 +0700

    config.py

---
+++ config.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+# === FILE KONFIGURASI ===
+# Simpan file ini sebagai config.py
+
+# --- Konfigurasi Email (dari skrip 1) ---
+EMAIL_ANDA = "emailmu@gmail.com"
+APP_PASSWORD_ANDA = "GANTI_DENGAN_APP_PASSWORD_BARU_ANDA" # Ganti dengan App Password BARU (16 digit)
+EMAIL_PENGIRIM_YANG_DICARI = "emailpengirim@gmail.com"
